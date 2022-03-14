e0a5ab73a8710595e62439be22504fe5e2f80d40
Ems <guesbaya@hotmail.fr>
2022-03-14 15:39:46 +0100

    created client-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule
