c0e13f48717f5967cf9f23bb2d29b411167efeb2
Ems <guesbaya@hotmail.fr>
2022-03-15 14:32:58 +0100

    javascript included via popper

--- src/app/client.ts
+++ src/app/client.ts
@@ -8,21 +8,21 @@ export class Client {
     // contacts: Array<Contact>;
 
 
-  constructor(
-    id: number, 
-    raisonSociale: string, 
-    siren: string, 
-    dateCreation: Date, 
-    dateMAJ: Date, 
-    // contacts: Array<Contact>
-) {
-    this.id = id
-    this.raisonSociale = raisonSociale
-    this.siren = siren
-    this.dateCreation = dateCreation
-    this.dateMAJ = dateMAJ
-    // this.contacts = contacts
-  }
+//   constructor(
+//     // id: number, 
+//     // raisonSociale: string, 
+//     // siren: string, 
+//     // dateCreation: Date, 
+//     // dateMAJ: Date, 
+//     // // contacts: Array<Contact>
+// ) {
+//     this.id = id
+//     this.raisonSociale = raisonSociale
+//     this.siren = siren
+//     this.dateCreation = dateCreation
+//     this.dateMAJ = dateMAJ
+//     // this.contacts = contacts
+//   }
 
 
 }
--- src/app/create-client/create-client.component.ts
+++ src/app/create-client/create-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-create-client',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-client.component.css']
 })
 export class CreateClientComponent implements OnInit {
+
+  client: Client = new Client();
 
   constructor() { }
 
