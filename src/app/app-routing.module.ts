b0ace765d0b82e7cae13696f0d59eee3ac5ef002
eguesbaya <guesbaya@hotmail.fr>
2022-03-17 15:58:35 +0100

    routing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { ClientListComponent } from "./client-list/client-list.component";
 import { ContactListComponent } from "./contact-list/contact-list.component";
 import { CreateClientComponent } from "./create-client/create-client.component";
+import { UpdateClientComponent } from "./update-client/update-client.component";
 import { ViewClientComponent } from "./view-client/view-client.component";
 
 const routes: Routes = [
     { path: "clients", component: ClientListComponent },
     { path: "create-client", component: CreateClientComponent },
-    { path: "view-client/:id", component: ViewClientComponent},
-    { path: "contacts", component: ContactListComponent}
+    { path: "view-client/:id", component: ViewClientComponent },
+    { path: "update-client/:id", component: UpdateClientComponent },
+    { path: "contacts", component: ContactListComponent }
 ];
 
 @NgModule({
