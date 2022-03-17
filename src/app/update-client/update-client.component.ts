5b9505b99888fd25fed2c0e9bceb3bf0d6c6d248
eguesbaya <guesbaya@hotmail.fr>
2022-03-17 16:04:16 +0100

    init update-client ok

--- src/app/update-client/update-client.component.ts
+++ src/app/update-client/update-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from '../client';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-update-client',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateClientComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  client: Client;
+  
+  constructor(private clientService: ClientService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.clientService.getClientById(this.id).subscribe(data => {
+      this.client = data;
+    })
   }
 
 }
